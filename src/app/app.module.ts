473d346babb437e56cf8c9aee93ec3470f511da8
jegadeeswaran-thangarasu <jegadeeswaranta7@gmail.com>
2021-06-12 20:11:39 +0530

    added add-item-form, budget-item-list, budget-item-card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { AddItemFormComponent } from './components/add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './components/budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './components/budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/budget-item-list/budget-item-list.component.ts
@@ -0,0 +1,16 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-item-list',
+  templateUrl: './budget-item-list.component.html',
+  styleUrls: ['./budget-item-list.component.scss']
+})
+export class BudgetItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
